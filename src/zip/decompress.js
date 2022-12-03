fae246fe0be17e5728023f1918f2bd8aeff9a7c4
kchrgn <kchrgn@gmail.com>
2022-12-03 22:23:28 +0300

    feat: implement zip/decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs'
+import { access } from 'fs/promises';
+import { createGunzip } from 'zlib'
+import { pipeline } from 'stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const archFile = new URL('./files/archive.gz', import.meta.url);
+    const dstFile = new URL('./files/fileToCompress.txt', import.meta.url);
+
+    try {
+        await access(archFile);
+    } catch (err) {
+        throw err;
+    }
+
+    pipeline (
+        createReadStream(archFile),
+        createGunzip(),
+        createWriteStream(dstFile),
+        err => {
+            if (err) throw err;
+        }
+    )
 };
 
 await decompress();

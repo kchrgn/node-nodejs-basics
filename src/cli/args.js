23a78151a6980032d23844cc49bcafe1d3777154
kchrgn <kchrgn@gmail.com>
2022-12-03 16:56:17 +0300

    feat: implement cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    let res = '';
+    for (let i = 2; i < process.argv.length; i += 1) {
+        if (process.argv[i].slice(0, 2) === '--') {
+            if (res) res += ', ';
+            res += process.argv[i].slice(2) + ' is ' + process.argv[i + 1];
+        }
+    }
+    console.log(res);
 };
 
 parseArgs();

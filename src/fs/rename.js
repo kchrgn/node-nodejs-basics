ef59f6d1e204be4972abe2e8c92283f9a236980c
kchrgn <kchrgn@gmail.com>
2022-12-03 12:27:23 +0300

    feat: implement fs/rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,10 +1,6 @@
 import {rename as renameFile, access} from 'fs/promises'
 
 const rename = async () => {
-    // Write your code here 
-    //rename.js - implement function that renames file wrongFilename.txt
-    // to properFilename with extension .md (if there's no file wrongFilename.txt
-    // or properFilename.md already exists Error with message FS operation failed must be thrown)
     const oldFileName = new URL('./files/wrongFilename.txt', import.meta.url)
     const newFileName = new URL('./files/properFilename.md', import.meta.url)
 

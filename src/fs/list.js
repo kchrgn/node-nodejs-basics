656f7ed8d734126b4c6af647bb38b6a5ee6c6904
kchrgn <kchrgn@gmail.com>
2022-12-03 13:03:06 +0300

    feat: implement fs/list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import {readdir} from 'fs/promises'
+
 const list = async () => {
     // Write your code here 
+    // list.js - implement function that prints all array of filenames from files folder into console
+    // (if files folder doesn't exists Error with message FS operation failed must be thrown)
+    const filesFolderPath = new URL('./files', import.meta.url);
+
+    try {
+        const fileNameList = await readdir(filesFolderPath);
+        console.log (fileNameList);
+    } catch (err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();

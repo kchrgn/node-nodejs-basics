4388a574a4132b5dc111f127a2609a1670204bf3
kchrgn <kchrgn@gmail.com>
2022-12-03 21:57:19 +0300

    feat: implement zip/compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from 'fs'
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+    const srcFile = new URL('./files/fileToCompress.txt', import.meta.url);
+    const archFile = new URL('./files/archive.gz', import.meta.url);
+
+    pipeline (
+        createReadStream(srcFile),
+        createGzip(),
+        createWriteStream(archFile),
+        err => {
+            if (err) throw err;
+        }
+    )
 };
 
 await compress();

8e91b2e81333d30e2772c0e5e6d5577883533184
kchrgn <kchrgn@gmail.com>
2022-12-03 11:12:29 +0300

    refactor: change imports, __filename, __dirname in modules/cjsToEsm task

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,36 +1,31 @@
-// const path = require('path');
-import * as path from 'path';
-// const { release, version } = require('os');
-import { release, version } from 'os';
-// const { createServer: createServerHttp } = require('http');
-import {createServer as createServerHttp} from 'http';
-// require('./files/c');
-import './files/c.js';
-import * as url from 'url'
-import { assert } from 'console';
+import * as path from 'path'
+import { release, version } from 'os'
+import {createServer as createServerHttp} from 'http'
+import './files/c.js'
+import {fileURLToPath} from 'url'
+import { createRequire } from 'module';
 
 const random = Math.random();
 
-export let unknownObject;
+let unknownObject;
 
+const require = createRequire(import.meta.url);
 if (random > 0.5) {
-    // unknownObject = require('./files/a.json');
-    unknownObject = await import('./files/a.json', {assert: {type: 'json'}});
+    unknownObject = require('./files/a.json');
 } else {
-    // unknownObject = require('./files/b.json');
-    unknownObject = await import('./files/b.json', {assert: {type: 'json'}});
+    unknownObject = require('./files/b.json');
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-export const myServer = createServerHttp((_, res) => {
+const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
@@ -42,4 +37,6 @@ myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log('To terminate it, use Ctrl+C combination');
 });
+
+export {unknownObject, myServer};
 

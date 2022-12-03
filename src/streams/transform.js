99397c1f7d3ef4e579b5c128f7de679f5bc1ad18
kchrgn <kchrgn@gmail.com>
2022-12-03 18:25:11 +0300

    feat: implement streams/transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import {stdin, stdout} from 'process'
+import {Transform, pipeline} from 'stream'
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            this.push(chunk.toString().trim().split('').reverse().join('') + '\n');
+            callback();
+        }
+    });
+    pipeline(
+        stdin,
+        reverse,
+        stdout,
+        err => {throw err}
+    )
 };
 
 await transform();
